6adb4bcd17608a70a6c58d3d3b7acc31a9938cae
abdal09 <71551618+abdal09@users.noreply.github.com>
2020-09-23 20:29:59 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,28 @@
+ #include <stdio.h>
+
+ int main() 
+     
+{
+    puts("                                        Jawahar Navodaya Vidyalaya \n ");
+    puts("                                           Anual Report card \n");
+    puts("Name:Sriyank Siddharta\N");
+    puts("Standard:10\n");
+    puts("Section:A\n");
+                
+    puts("                                          Marks secured out of 100 \n");
+   
+   
+   puts("MAthematics :93\n");
+   puts("English :74\n");
+   puts("Hindi :85\n");
+   puts("Science :97\n");
+   puts("Social Science :85\n");
+   
+   puts("Total marks secured :430\n");
+   
+   
+    puts(         "GRADE :B");
+ return 0;
+}
+   
+   
