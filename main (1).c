9eb9e3d33c3e890b9d431822ee55481d38acd5c8
abdal09 <71551618+abdal09@users.noreply.github.com>
2020-09-23 20:28:51 +0530

    Add files via upload

---
+++ main (1).c
@@ -0,0 +1,27 @@
+/*
+Author:Intranshala
+Module:Getting Started with programing C   
+Topic:Tokens
+*/
+
+#include <stdio.h>
+
+int main() {
+    char name[15] ="Farm House";
+    char size = 'M';
+    int personItServes = 2;
+    float retailPrice = 365.8;
+ 
+ printf("%s", name);
+ 
+ printf("%c", size);
+ 
+ printf("%d", personItServes);
+ 
+ printf("%f", retailPrice);
+    
+return 0;
+    
+}
+
+
